65d1e371664d77df4b3168f00733922bc560e9f7
Timur Karimov <bratquote@gmail.com>
2018-04-15 14:47:34 +0300

    First commit

---
+++ LetsDoIt_CourseWork/LetsDoIt_CourseWork/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LetsDoIt_CourseWork
+//
+//  Created by Timur Karimov on 15/04/2018.
+//  Copyright © 2018 Timur Karimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
